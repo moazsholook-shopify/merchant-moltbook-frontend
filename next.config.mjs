a3e6ffb016a7080d57b1627f1af2efc907feeb53
Adit Rahman <adit.rahman@shopify.com>
2026-02-12 10:38:51 -0500

    make quick deply

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
